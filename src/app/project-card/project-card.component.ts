65fab613325125aa3ce96d11ad061c4dd19d1bb6
Kyi Sin Thant <kyisinthant@innova.co.th>
2024-06-28 13:39:21 +0700

    add portfolio+ project card

--- src/app/project-card/project-card.component.ts
+++ src/app/project-card/project-card.component.ts
@@ -1,12 +1,30 @@
-import { Component, Input } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 import { Project } from '../_models/Project';
+import { ProjectsService } from '../_services/projects.service';
+declare var bootstrap: any;
 
 @Component({
   selector: 'app-project-card',
   templateUrl: './project-card.component.html',
   styleUrl: './project-card.component.css'
 })
-export class ProjectCardComponent {
+
+export class ProjectCardComponent{
   @Input() project= {} as Project;
+
+  constructor(private _projectService: ProjectsService){
+    
+  }
+
+  sendData(id: any){
+      const modal = document.getElementById('projectModal-' + id);
+      if (modal) {
+        const bootstrapModal = new bootstrap.Modal(modal);
+        bootstrapModal.show();
+      } else {
+        console.error('Modal element not found.');
+      }
+  }
+
 
 }
